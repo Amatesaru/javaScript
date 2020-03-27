69a956d7ab9e6f99686c041abf9969196d91dc2b
Oye <62082226+Amatesaru@users.noreply.github.com>
2020-03-27 22:53:47 +0100

    my code

---
+++ mycode.js
@@ -0,0 +1,9 @@
+var myName = 'Oyeyinka';
+console.log(myName)
+var myCourses = ['FrontEnd', 'BackEnd', 'Mobile'];
+myCourses.toString();
+console.log(myCourses.toString());
+for (var i = 1; i <= 200; i++)
+    if (i % 2 !== 0)
+    
+    console.log(i);
